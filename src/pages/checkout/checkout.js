0bb69d629ccc848884ab15856bc776a0a0c9f755
Mrsumpalumpa <marceloct1986@gmail>
2022-01-31 19:42:58 +0100

    omcat

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Checkout() {
   return (
     <div>
-      <h1 style={{fontSize:'300px',marginTop:'10vh'}} className='text-center'>DAME TU DINELO</h1>
+      <h1 style={{fontSize:'25vmin',marginTop:'15vh'}} className='text-center'>DAME TU DINELO</h1>
     </div>
   )
 }
